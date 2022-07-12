fa993e6fdd5fc25f0e108a6c1c82d3b4b8de826c
vexe <sadbhavadhikari@gmail.com>
2022-07-12 16:53:17 +0545

    [added] todo structure

---
+++ src/lib/TodoItem.svelte
@@ -0,0 +1,15 @@
+<div>
+    <form action="" method="">
+        <input type="hidden" name="done" />
+        <button aria-label="mark as done or not done">done/not</button>
+    </form>
+
+    <form action="" method="">
+        <input type="text" />
+        <button aria-label="save todo">save</button>
+    </form>
+
+    <form action="" method="">
+        <button aria-label="delete todo">annihilate</button>
+    </form>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,19 @@
-<h1>Todos</h1>
+<script lang="ts">
+    import TodoItem from "../lib/TodoItem.svelte";
 
-<form action="" method="">
-    <input type="text" name="text" aria-label="add a todo" placeholder="add a todo" />
-</form>
+    const title = "Todos";
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
+
+<main>
+    <h1>{title}</h1>
+
+    <form action="" method="">
+        <input type="text" name="text" aria-label="add a todo" placeholder="add a todo" />
+    </form>
+
+    <TodoItem />
+</main>
