e13178df3d6fdb9ac56fff78337a058a34763481
vexe <vexebusiness6472814@gmail.com>
2022-07-17 15:01:39 +0545

    [added] prisma & init migration

--- src/routes/todos/index.json.ts
+++ src/routes/todos/index.json.ts
@@ -9,7 +9,6 @@ export const post: RequestHandler = async (reqEvent) => {
     const formData = await reqEvent.request.formData();
 
     return api(reqEvent, {
-        uid: `${Date.now()}`, // TODO: replace with uid from db
         created_at: new Date(),
         text: formData.get("text") as string,
         done: false
